da7f13d6a6cac845407ad4870e40e901487d3245
csbadhe <csbadhe08@gmail.com>
2016-12-10 12:40:29 +0530

    Directory Uploaded

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+from random import randint
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def toss():
+    coin = randint(0,1)
+    return render_template('hello.html', spin=spin)
+
+
+app.run(debug=True)
